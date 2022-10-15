62c466f3d16969823b99270e01eea4e00931e235
Jesslerchip <jessicaseab17@gmail.com>
2022-10-15 12:38:23 -0500

    Added project structure and simple Hello World program.

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JOptionPane.showMessageDialog(null, "Hello, World!", "Hello World", JOptionPane.INFORMATION_MESSAGE);
+
+    }
+}
