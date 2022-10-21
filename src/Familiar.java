196ddc13f1130858408bde8f04f57d5b82c4b073
Jesslerchip <jessicaseab17@gmail.com>
2022-10-21 18:17:01 -0500

    Added Familiar class.

---
+++ src/Familiar.java
@@ -0,0 +1,13 @@
+public class Familiar {
+
+    private int level = 1;
+    private int xp = 0;
+    private int xpMax = 100;
+
+    // HP, Mana, Speed, Damage, Defense, Accuracy Modifiers
+    private double[] modifiers = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
+
+    // Flags
+    private boolean unlocked = false;
+
+}
